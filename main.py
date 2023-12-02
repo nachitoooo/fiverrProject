4a750eb454c34a5cd6bebbb29a9d1b4147830c43
nacho <ignacioaristegui025@gmail.com>
2023-12-01 22:14:34 -0300

    proyecto avance conectar red wifi

--- main.py
+++ main.py
@@ -3,46 +3,58 @@ import subprocess
 import re
 
 def scan_wifi_networks():
-    # Clear existing entries in the listbox
     wifi_listbox.delete(0, tk.END)
-
     try:
-        # Run the netsh command to get Wi-Fi information
         result = subprocess.run(["netsh", "wlan", "show", "network"], capture_output=True, text=True, check=True)
-
-        # Process the output to extract and display relevant information
         networks = re.findall(r"SSID \d+ : (.+)", result.stdout)
         for network in networks:
             wifi_listbox.insert(tk.END, network)
-
     except subprocess.CalledProcessError as e:
-        # Handle the error, for example, print the error message
-        print(f"Error: {e}")
+        print(f"Error scanning Wi-Fi networks: {e}")
 
-# Create the main window
+def connect_to_wifi():
+    selected_index = wifi_listbox.curselection()
+    if selected_index:
+        selected_network = wifi_listbox.get(selected_index)
+        password = password_entry.get()
+        
+        try:
+            subprocess.run(["netsh", "wlan", "connect", "name=" + selected_network], check=True)
+            print(f"Connected to {selected_network}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error connecting to {selected_network}: {e}")
+    else:
+        print("Please select a Wi-Fi network to connect.")
+
+def check_connection_successful():
+    try:
+        # Intenta realizar una operación que dependa de la conexión, por ejemplo, hacer ping a un servidor
+        subprocess.run(["ping", "-n", "1", "google.com"], check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
 root = tk.Tk()
 root.title("Wi-Fi Scanner")
 
-# Maximize the window
 root.state('zoomed')
 
-# Create a button to initiate Wi-Fi scanning
 scan_button = tk.Button(root, text="Scan Wi-Fi Networks", command=scan_wifi_networks)
 scan_button.pack(pady=10)
 
-# Create a vertical scrollbar
 scrollbar = tk.Scrollbar(root, orient=tk.VERTICAL)
-
-# Create a larger listbox to display the scanned Wi-Fi networks with scrollbar
 wifi_listbox = tk.Listbox(root, selectmode=tk.SINGLE, yscrollcommand=scrollbar.set, height=30, width=100)
 wifi_listbox.pack(pady=10)
-
-# Configure the scrollbar to work with the listbox
 scrollbar.config(command=wifi_listbox.yview)
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
-# Set the yscroll to the bottom to make the text start from the bottom
 wifi_listbox.yview(tk.END)
 
-# Start the Tkinter main loop
+password_label = tk.Label(root, text="Password:")
+password_label.pack(pady=5)
+password_entry = tk.Entry(root, show="*")
+password_entry.pack(pady=5)
+
+connect_button = tk.Button(root, text="Connect to Wi-Fi", command=connect_to_wifi)
+connect_button.pack(pady=10)
+
 root.mainloop()

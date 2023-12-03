0de2307e9efc6714444d53ffc639e3387b8a66b6
nacho <ignacioaristegui025@gmail.com>
2023-12-02 22:37:11 -0300

    conectar a una red a la que no me habia conectado antes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
-import subprocess
-import re
+import time
+import pywifi
 from customtkinter import *
 
 def show_error(message):
@@ -9,13 +9,16 @@ def show_error(message):
 
 def scan_wifi_networks():
     wifi_listbox.delete(0, tk.END)
-    try:
-        result = subprocess.run(["netsh", "wlan", "show", "network"], capture_output=True, text=True, check=True)
-        networks = re.findall(r"SSID \d+ : (.+)", result.stdout)
-        for network in networks:
-            wifi_listbox.insert(tk.END, network)
-    except subprocess.CalledProcessError as e:
-        show_error(f"Error scanning Wi-Fi networks: {e}")
+    wifi = pywifi.PyWiFi()
+    iface = wifi.interfaces()[0]
+    
+    iface.scan()
+    time.sleep(2)  # Espera unos segundos para que se complete la exploración
+    
+    scan_result = iface.scan_results()
+    
+    for result in scan_result:
+        wifi_listbox.insert(tk.END, result.ssid)
 
 def connect_to_wifi():
     selected_index = wifi_listbox.curselection()
@@ -23,28 +26,32 @@ def connect_to_wifi():
         selected_network = wifi_listbox.get(selected_index)
         password = password_entry.get()
         
-        try:
-            subprocess.run(["netsh", "wlan", "connect", "name=" + selected_network], check=True)
-            messagebox.showinfo("Success", f"Connected to {selected_network}")
-        except subprocess.CalledProcessError as e:
+        wifi = pywifi.PyWiFi()
+        iface = wifi.interfaces()[0]
+        
+        profile = pywifi.Profile()
+        profile.ssid = selected_network
+        profile.auth = pywifi.const.AUTH_ALG_OPEN
+        profile.akm.append(pywifi.const.AKM_TYPE_WPA2PSK)
+        profile.cipher = pywifi.const.CIPHER_TYPE_CCMP
+        profile.key = password
+        
+        iface.remove_all_network_profiles()
+        temp_profile = iface.add_network_profile(profile)
+        
+        iface.connect(temp_profile)
+        
+        time.sleep(5)  # Espera unos segundos para que se establezca la conexión
+        
+        if iface.status() == pywifi.const.IFACE_CONNECTED:
+            messagebox.showinfo("Conexión exitosa.", f"Conectado a {selected_network}")
+        else:
             show_error(selected_network)
     else:
-        show_error("Please select a Wi-Fi network to connect.")
-
-def show_error(selected_network):
-    # Mensaje personalizado en caso de error de conexión
-    error_message = f"Error conectándose a {selected_network}: No se puede conectar a la red. Por favor, verifique los datos."
-    messagebox.showerror("Error", error_message)
-
-def check_connection_successful():
-    try:
-        subprocess.run(["ping", "-n", "1", "google.com"], check=True)
-        return True
-    except subprocess.CalledProcessError:
-        return False
+        show_error("Por favor seleccione una red Wi-Fi para conectar.")
 
 root = CTk()
-root.title("Wi-Fi Scanner")
+root.title("Wi-Fi Escáner")
 
 root.state('zoomed')
 
@@ -58,12 +65,12 @@ scrollbar.config(command=wifi_listbox.yview)
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 wifi_listbox.yview(tk.END)
 
-password_label = CTkLabel(root, text="Password:")
+password_label = CTkLabel(root, text="Contraseña:")
 password_label.pack(pady=5)
 password_entry = CTkEntry(root, show="*")
 password_entry.pack(pady=5)
 
-connect_button = CTkButton(root, text="Connect to Wi-Fi", command=connect_to_wifi)
+connect_button = CTkButton(root, text="Conectar a Wi-Fi", command=connect_to_wifi)
 connect_button.pack(pady=10)
 
 root.mainloop()

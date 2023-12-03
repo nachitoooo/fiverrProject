d6233500df04502b605f5d50703082358ae996d2
nacho <ignacioaristegui025@gmail.com>
2023-12-02 23:28:45 -0300

    comienzo dashboard botones

--- main.py
+++ main.py
@@ -75,6 +75,28 @@ scrollbar = tk.Scrollbar(root, orient=tk.VERTICAL)
 wifi_listbox = tk.Listbox(root, selectmode=tk.SINGLE, yscrollcommand=scrollbar.set, height=30, width=100, fg="white", bg="#171718", justify="center", font=("Helvetica", "12", "bold"))
 wifi_listbox.pack(pady=10)
 wifi_listbox.yview(tk.END)
+class CTkToolbar(tk.Frame):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.buttons = []
+
+    def add_button(self, button):
+        button.pack(side=tk.LEFT, padx=5)
+        self.buttons.append(button)
+
+main_toolbar = CTkToolbar(root, bg="#171718")
+main_toolbar.pack(side=tk.TOP, fill=tk.X)
+
+dashboard_button = CTkButton(main_toolbar, text="Dashboard")
+monitor_button = CTkButton(main_toolbar, text="Monitor")
+storage_button = CTkButton(main_toolbar, text="Almacenamiento" )
+settings_button = CTkButton(main_toolbar, text="Configuración")
+
+# Añadir botones a la barra principal
+main_toolbar.add_button(dashboard_button)
+main_toolbar.add_button(monitor_button)
+main_toolbar.add_button(storage_button)
+main_toolbar.add_button(settings_button)
 wifi_listbox.bind("<Double-Button-1>", on_wifi_double_click)
 
 root.mainloop()
